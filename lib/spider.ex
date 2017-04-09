a073c792441260bff4928f1060905315105a2cf0
haozi <haozi>
2017-04-09 11:34:42 +0800

    init

---
+++ lib/spider.ex
@@ -0,0 +1,18 @@
+defmodule Spider do
+  @moduledoc """
+  Documentation for Spider.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Spider.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
